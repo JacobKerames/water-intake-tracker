e0910af148dc6e9691612315a574495c2fc46ab6
Jacob Kerames <108101472+JacobKerames@users.noreply.github.com>
2023-03-12 15:47:20 -0600

    Partial working

--- app/src/main/java/com/example/watertracker/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/watertracker/ui/home/HomeFragment.java
@@ -19,7 +19,7 @@ import com.example.watertracker.CustomViewModelFactory;
 import com.example.watertracker.databinding.FragmentHomeBinding;
 
 public class HomeFragment extends Fragment {
-
+    // Declare class variables
     private FragmentHomeBinding binding;
     private HomeViewModel homeViewModel;
     private TextView waterIntakeTextView;
@@ -28,30 +28,40 @@ public class HomeFragment extends Fragment {
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
+        // Inflate the layout for this fragment using the FragmentHomeBinding class
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View rootView = binding.getRoot();
 
+        // Get the HomeViewModel using a custom ViewModelFactory and attach an observer to the recommended intake LiveData object
         homeViewModel = new ViewModelProvider(this, new CustomViewModelFactory(getActivity())).get(HomeViewModel.class);
         homeViewModel.getRecommendedIntake().observe(getViewLifecycleOwner(), recommendedIntake -> {
-            // update progress bar and text view with new recommended intake value
-            int progress = Math.min(homeViewModel.getWaterIntake().getValue() * 100 / recommendedIntake, 100);
-            waterProgressBar.setProgress(progress);
+            // Calculate the progress of the water intake as a percentage of the recommended intake and update the ProgressBar
+            if (homeViewModel != null && homeViewModel.getWaterIntake() != null) {
+                int progress = Math.min(homeViewModel.getWaterIntake().getValue() * 100 / recommendedIntake, 100);
+                waterProgressBar.setProgress(progress);
+            }
         });
 
+        // Get references to the views in the layout
         waterIntakeTextView = binding.waterIntakeTextView;
         waterProgressBar = binding.waterProgressBar;
         ozToAddEditText = binding.ozToAddEditText;
         Button addButton = binding.addButton;
 
+        // Attach a click listener to the add button
         addButton.setOnClickListener(view -> {
+            // Get the number of ounces to add from the EditText and add it to the water intake
             int ozToAdd = Integer.parseInt(ozToAddEditText.getText().toString());
             homeViewModel.addWaterIntake(ozToAdd, getContext());
-            waterIntakeTextView.setText(homeViewModel.getText().getValue());
-            int recommendedIntake = homeViewModel.getRecommendedIntake().getValue();
-            int progress = Math.min(homeViewModel.getWaterIntake().getValue() * 100 / recommendedIntake, 100);
-            waterProgressBar.setProgress(progress);
 
-            // Clear EditText and hide keyboard
+            // Update the water intake TextView and ProgressBar
+            waterIntakeTextView.setText(homeViewModel.getText().getValue());
+            if (homeViewModel != null && homeViewModel.getRecommendedIntake() != null) {
+                int progress = Math.min(homeViewModel.getWaterIntake().getValue() * 100 / homeViewModel.getRecommendedIntake().getValue(), 100);
+                waterProgressBar.setProgress(progress);
+            }
+
+            // Clear the EditText and hide the keyboard
             ozToAddEditText.getText().clear();
             InputMethodManager imm = (InputMethodManager) requireActivity().getSystemService(Context.INPUT_METHOD_SERVICE);
             imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
@@ -59,6 +69,7 @@ public class HomeFragment extends Fragment {
         return rootView;
     }
 
+    // Update the water intake TextView and ProgressBar when the fragment resumes
     @Override
     public void onResume() {
         super.onResume();
@@ -69,6 +80,7 @@ public class HomeFragment extends Fragment {
         }
     }
 
+    // Release binding resources when the fragment is destroyed
     @Override
     public void onDestroyView() {
         super.onDestroyView();
