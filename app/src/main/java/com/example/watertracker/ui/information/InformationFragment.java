7f8a8062e00227799ac2983e14d2070809f32ae8
Jacob Kerames <108101472+JacobKerames@users.noreply.github.com>
2023-03-12 05:21:31 -0600

    Initial

---
+++ app/src/main/java/com/example/watertracker/ui/information/InformationFragment.java
@@ -0,0 +1,37 @@
+package com.example.watertracker.ui.information;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.example.watertracker.databinding.FragmentInformationBinding;
+
+public class InformationFragment extends Fragment {
+
+    private FragmentInformationBinding binding;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        InformationViewModel dashboardViewModel =
+                new ViewModelProvider(this).get(InformationViewModel.class);
+
+        binding = FragmentInformationBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        final TextView textView = binding.textInformation;
+        dashboardViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        return root;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
